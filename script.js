66b0fd3567a8a0a658654ae5f7ac819a6d4dd477
Valerie Kucher <passechnik214@gmail.com>
2022-12-18 00:49:12 +0200

    create an object

---
+++ script.js
@@ -0,0 +1,11 @@
+// об'єкт, що міститиме інфу про автомобілі
+let car = {
+    madeBy: "Toyota Motor Corpotation",
+    model: "RAV4",
+    year: 2020,
+    averageSpeed: 150,
+    fuelTankVolume: 50,
+    averageFuelConsumption: 6.8,
+}
+
+console.log(Object.getOwnPropertyNames(car));
