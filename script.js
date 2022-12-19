3901b4ab98db422af948a180cb0194a62e3eb566
Valerie Kucher <passechnik214@gmail.com>
2022-12-19 18:25:35 +0200

    add minimum tasks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // об'єкт, що міститиме інфу про автомобілі
-let car = {
+const car = {
     madeBy: "Toyota Motor Corpotation",
     model: "RAV4",
     year: 2020,
@@ -7,5 +7,25 @@ let car = {
     fuelTankVolume: 50,
     averageFuelConsumption: 6.8,
 }
-
+//вивидення всієї інфи про об'єкт
 console.log(Object.getOwnPropertyNames(car));
+
+//додавання ім'я водія у список
+car.driver = "John";
+
+// перевірка водія на наявність його імені у списку
+
+//вирахувати час для подолання переданої відстані, враховуючи зупинки на одну годину кожні чотири години
+function findTripTime(distance) {
+    let timeWithoutStops = Math.round(Number(distance / car.averageSpeed));
+    console.log(timeWithoutStops);
+    let rest = findTimeWithStops(timeWithoutStops);
+    console.log(rest);
+    return timeWithoutStops + rest;
+}
+
+function findTimeWithStops(timeWithoutStops) {
+    let hoursBetweenStops = 4;
+    return Math.floor(timeWithoutStops / hoursBetweenStops);
+}
+
